2655563ca855840f328c73ea98c5b6ce1d5e2feb
hetao_awa <3112871060@qq.com>
2026-05-17 16:56:02 +0800

    fix: 更新 release 版本代理到本机

--- vite.config.js
+++ vite.config.js
@@ -19,7 +19,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:9860',
+        target: 'http://localhost',
         changeOrigin: true,
       },
     },
